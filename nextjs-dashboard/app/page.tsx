41c8289a6dc0efa40a564e94c77e3977b2bb201c
Prachanda Rana <62231260+prachanda482244@users.noreply.github.com>
2024-02-18 19:10:45 +0545

    routes and layout in next js

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -28,9 +28,9 @@ export default function Page() {
           </p>
           <Link
             href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors  hover:bg-blue-400 md:text-base"
           >
-            <span>Log in</span> <ArrowRightIcon/>
+            <span >Log in</span> <ArrowRightIcon />
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
